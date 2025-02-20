885d63dd4590edade40f7aceb72295ada977080a
Zorila Laurentiu <laurennntiu.zorila@gmail.com>
2025-02-20 20:42:08 +0200

    Adaugat functionalitatea cosului de cumparaturi

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from itertools import product
+
+from cart import Cart
 from product import Product
 from product_manager import ProductManager
 
@@ -7,3 +10,17 @@ product_manager.add_product(Product("Telefon", 2000, 10))
 product_manager.add_product(Product("Căști", 150, 20))
 
 product_manager.display_products()
+
+print(f"Valoarea totala a inventarului: {product_manager.total_inventory_value()} RON")
+
+# Instantiate Cart
+cart = Cart()
+
+# Add products in cart
+cart.add_to_cart(product_manager.products[0])
+cart.add_to_cart(product_manager.products[1])
+cart.add_to_cart(product_manager.products[2])
+
+# Show cart items
+cart.display_cart()
+print(f"Total de plata: {cart.cart_total()} RON")
